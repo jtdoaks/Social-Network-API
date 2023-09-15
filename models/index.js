aef69952feca847e85e0ae6ea21925ed7bae0c2c
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-09-15 09:54:20 -0400

    created index file in models and required the models in server.

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Email = require('./Email');
+const User = require('./User');
+const Thoughts = require('./Thoughts');
+const Reaction = require('./Reaction');
+
+
+module.exports = { Email, User, Thoughts, Reaction };
